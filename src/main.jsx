9d0ee5715016ec9ec2f0c4c8111618ae3bda327c
Phuc <duyphuclenguyen@gmail.com>
2024-10-23 21:53:15 -0400

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './E-Plant website/src/App.jsx'
+import {Provider} from 'react-redux'
+import store from './Components/store.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>,
 )
+
+/*Import the Provider component from the react-redux library, which allows the Redux store to be accessed by all components in the application.
+
+Import the Redux store from the store.js file.
+
+Wrap the App with the Provider component and pass the Redux store as a prop. This allows every component access to the details of store.js.*/
